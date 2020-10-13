3fade1fc35a51f283f5c47c01995030bc4dacf82
Tomas R Moreno P <sirlordt@gmail.com>
2020-10-13 12:40:25 -0700

    branch: backend-server-01, new functions in CommonUtilities.ts, SystemUtilities.ts

--- src/04_test/jest/SystemUtilities.spec.ts
+++ src/04_test/jest/SystemUtilities.spec.ts
@@ -182,7 +182,7 @@ test( `getRandomIntegerRangeFromString 007`, async () => {
 
 test( `getCurrentDateAndTimeFromAndIncMinutes 001`, async () => {
 
-  const incrementedDateAndTime = SystemUtilities.getCurrentDateAndTimeFromAndIncMinutes( '2020-01-01 07:00:00', 1 );
+  const incrementedDateAndTime = SystemUtilities.getCurrentDateAndTimeFromAndIncMinutes( "2020-01-01 07:00:00", 1 );
 
   const formattedDateAndTime = incrementedDateAndTime.format( CommonConstants._DATE_TIME_LONG_FORMAT_05 );
 
@@ -192,7 +192,7 @@ test( `getCurrentDateAndTimeFromAndIncMinutes 001`, async () => {
 
 test( `getCurrentDateAndTimeFromAndIncMinutes 002`, async () => {
 
-  const incrementedDateAndTime = SystemUtilities.getCurrentDateAndTimeFromAndIncMinutes( '2020-01-01 07:10:00', 10 );
+  const incrementedDateAndTime = SystemUtilities.getCurrentDateAndTimeFromAndIncMinutes( "2020-01-01 07:10:00", 10 );
 
   const formattedDateAndTime = incrementedDateAndTime.format( CommonConstants._DATE_TIME_LONG_FORMAT_05 );
 
@@ -202,7 +202,7 @@ test( `getCurrentDateAndTimeFromAndIncMinutes 002`, async () => {
 
 test( `getCurrentDateAndTimeFromAndDecMinutes 001`, async () => {
 
-  const incrementedDateAndTime = SystemUtilities.getCurrentDateAndTimeFromAndDecMinutes( '2020-01-01 07:00:00', 1 );
+  const incrementedDateAndTime = SystemUtilities.getCurrentDateAndTimeFromAndDecMinutes( "2020-01-01 07:00:00", 1 );
 
   const formattedDateAndTime = incrementedDateAndTime.format( CommonConstants._DATE_TIME_LONG_FORMAT_05 );
 
@@ -212,10 +212,170 @@ test( `getCurrentDateAndTimeFromAndDecMinutes 001`, async () => {
 
 test( `getCurrentDateAndTimeFromAndDecMinutes 002`, async () => {
 
-  const incrementedDateAndTime = SystemUtilities.getCurrentDateAndTimeFromAndDecMinutes( '2020-01-01 07:10:00', 10 );
+  const incrementedDateAndTime = SystemUtilities.getCurrentDateAndTimeFromAndDecMinutes( "2020-01-01 07:10:00", 10 );
 
   const formattedDateAndTime = incrementedDateAndTime.format( CommonConstants._DATE_TIME_LONG_FORMAT_05 );
 
   expect( formattedDateAndTime ).toEqual( "2020-01-01T07:00:00" );
+
+})
+
+test( `isDateAndTimeBeforeDays 001`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtDays( "2020-10-13 07:10:00", 1, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( true );
+
+})
+
+test( `isDateAndTimeBeforeDays 002`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtDays( "2020-10-14 07:10:00", 1, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( true );
+
+})
+
+test( `isDateAndTimeBeforeDays 003`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtDays( "2020-10-15 07:10:00", 1, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( false );
+
+})
+
+test( `isDateAndTimeBeforeDays 004`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtDays( "2020-10-16 07:10:00", 1, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( false );
+
+})
+
+test( `isDateAndTimeBeforeDays 005`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtHours( "2020-10-12 07:10:00", 1, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( false );
+
+})
+
+test( `isDateAndTimeBeforeHours 001`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtHours( "2020-10-13 07:10:00", 1, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( true );
+
+})
+
+test( `isDateAndTimeBeforeHours 002`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtHours( "2020-10-13 08:09:00", 1, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( true );
+
+})
+
+test( `isDateAndTimeBeforeHours 003`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtHours( "2020-10-13 08:11:00", 1, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( false );
+
+})
+
+test( `isDateAndTimeBeforeHours 004`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtHours( "2020-10-13 09:09:00", 1, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( false );
+
+})
+
+test( `isDateAndTimeBeforeHours 005`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtHours( "2020-10-13 06:10:00", 1, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( false );
+
+})
+
+test( `isDateAndTimeBeforeMinutes 001`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtMinutes( "2020-10-13 07:10:00", 10, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( true );
+
+})
+
+test( `isDateAndTimeBeforeMinutes 002`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtMinutes( "2020-10-13 07:20:00", 10, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( true );
+
+})
+
+test( `isDateAndTimeBeforeMinutes 003`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtMinutes( "2020-10-13 07:30:00", 10, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( false );
+
+})
+
+test( `isDateAndTimeBeforeMinutes 004`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtMinutes( "2020-10-13 08:00:00", 10, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( false );
+
+})
+
+test( `isDateAndTimeBeforeMinutes 005`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtMinutes( "2020-10-13 06:59:00", 10, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( false );
+
+})
+
+test( `isDateAndTimeBeforeSeconds 001`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtSeconds( "2020-10-13 07:10:00", 10, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( true );
+
+})
+
+test( `isDateAndTimeBeforeSeconds 002`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtSeconds( "2020-10-13 07:10:10", 10, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( true );
+
+})
+
+test( `isDateAndTimeBeforeSeconds 003`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtSeconds( "2020-10-13 07:10:11", 10, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( false );
+
+})
+
+test( `isDateAndTimeBeforeSeconds 004`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtSeconds( "2020-10-13 07:10:30", 10, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( false );
+
+})
+
+test( `isDateAndTimeBeforeSeconds 005`, async () => {
+
+  const bIsBefore = SystemUtilities.isDateAndTimeBeforeAtSeconds( "2020-10-13 07:09:59", 10, "2020-10-13 07:10:00" );
+
+  expect( bIsBefore ).toBe( false );
 
 })
